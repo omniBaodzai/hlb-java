281e8cee4bb898d5eb8c666d5c533be241d19ab8
omniBaodzai <huynhlebaoes@gmail.com>
2026-03-28 16:34:16 +0700

    lesson 23 exercise

---
+++ udemy/exercises/less23-challenge/ChallengeChar.java
@@ -0,0 +1,12 @@
+public class ChallengeChar
+{
+    public static void main(String[] arg)
+    {
+        char mySimpleChar = '?';
+        char myUnicodeChar = '\u003f';
+        char myDecimalChar = 63;
+
+        System.out.print("My values are = " + mySimpleChar + myUnicodeChar + myDecimalChar);
+    }
+
+}
