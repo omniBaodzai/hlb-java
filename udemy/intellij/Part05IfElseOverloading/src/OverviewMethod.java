24379da099153cd4f05336731cec67e521eb0001
Huynh Le Bao <huynhlebaoes@gmail.com>
2026-04-04 10:53:01 +0700

    Lesson 48: Overview method

---
+++ udemy/intellij/Part05IfElseOverloading/src/OverviewMethod.java
@@ -0,0 +1,44 @@
+public class OverviewMethod {
+    public static void main(String[] args)
+    {
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        int finalScore = score;
+
+        // Gọi / thực thi phương thức
+        calculateScore();
+
+        // New value
+        score = 10000;
+        levelCompleted = 8;
+        bonus = 200;
+
+        finalScore = score;
+
+        if (gameOver)
+        {
+            finalScore += (levelCompleted * bonus);
+            System.out.println("2. Your final score was " + finalScore);
+        }
+    }
+    // method
+    public static void calculateScore()
+    {
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        int finalScore = score;
+
+        if (gameOver)
+        {
+            finalScore += (levelCompleted * bonus);
+            finalScore += 1000;
+            System.out.println("1. Your final score was " + finalScore);
+        }
+    }
+}
